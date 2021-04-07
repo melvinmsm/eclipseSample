9992fb3e6fc90128de65adefec60a9a480bc8af6
melvinmathew <melvinmathew@melvinmathew.Nagarro.local>
2021-04-07 09:32:49 +0530

    first commit

---
+++ Sample/src/add.java
@@ -0,0 +1,10 @@
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=2, b=24;
+		System.out.println(a+b);
+	}
+
+}
---
+++ Sample/src/sub.java
@@ -0,0 +1,10 @@
+
+public class sub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=20, y=10;
+		System.out.println(x-y);
+	}
+
+}
